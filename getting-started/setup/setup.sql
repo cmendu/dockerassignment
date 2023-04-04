b6a6883a0ca6b008a406b311673c6295701cc119
cmendu <cmendu@teksystems.com>
2023-04-04 15:03:57 +0000

    start

---
+++ getting-started/setup/setup.sql
@@ -0,0 +1,8 @@
+--Create SquareMetric
+CREATE TABLE square_metric(square_metric DECIMAL DEFAULT NULL);
+
+--Create CubeMetric
+CREATE TABLE cube_metric(cube_metric DECIMAL DEFAULT NULL);
+
+--Create FibonacciMetric
+CREATE TABLE fibonacci_metrics(fibonacci_metrics DECIMAL DEFAULT NULL);
