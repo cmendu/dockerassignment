5a66e8c0c26a60612378159eb16e8bfec5c06576
cmendu <cmendu@teksystems.com>
2023-04-05 17:21:26 +0000

    start

--- calculator/cube/cube.py
+++ calculator/cube/cube.py
@@ -3,11 +3,11 @@ from flask import Flask, jsonify
 app = Flask(__name__)
   
 # Pass the required route to the decorator.
-@app.route("cube")
-def square():
+@app.route("/cube")
+def cube():
     i = int(request.args.get('i'))
     square = i*i*i
-    return jsonify('cube':cube)
+    return jsonify({'result': cube})
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- calculator/square/square.py
+++ calculator/square/square.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def square():
     i = int(request.args.get('i'))
     square = i*i
-    return jsonify('square':square)
+    return jsonify({'result': square})
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
