544a7ac1ed2493c87e983f521ac161538f8784d8
cmendu <cmendu@teksystems.com>
2023-04-06 09:25:52 +0000

    start

--- calculator/cube/cube.py
+++ calculator/cube/cube.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import mysql.connector;
   
 app = Flask(__name__)
   
@@ -6,8 +7,25 @@ app = Flask(__name__)
 @app.route("/cube")
 def cube():
     i = int(request.args.get('i'))
-    square = i*i*i
+    cube = i*i*i
+    
+    mydb = mysql.connector.connect(
+    host="172.19.0.3",
+    user="docker",
+    password="secret",
+    database="MathCalc"
+        )
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO cube_metric (cube_metric) VALUES (%s)"
+    val = (cube)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    
     return jsonify({'result': cube})
+    
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
